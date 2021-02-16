436c6470b442fe5186f18c30345075fef667c065
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2021-02-16 22:56:28 +0100

    Scheduled function to get releases

--- functions/index.js
+++ functions/index.js
@@ -11,32 +11,17 @@ const supabase = createClient(
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest(async (request, response) => {
-  const { data, error } = await supabase.from("topic").select("*");
+exports.refreshReleases = functions.region('europe-west1').https.onRequest(
+  async (request, response) => {
+    await getNewReleases();
+    response.send("hi");
+  }
+);
 
-  await asyncForEach(data, async (topic) => {
-    const scmUrlSplit = topic.info.scm.url.split("/");
-    const owner = scmUrlSplit[scmUrlSplit.length - 2];
-    const repo = scmUrlSplit[scmUrlSplit.length - 1];
-
-    const {data} = await octokit.repos.listReleases({
-      owner: "JetBrains",
-      repo: "kotlin",
-    });
-
-    functions.logger.info("Hello logs!", { owner, repo });
-    data.forEach(release => functions.logger.info(release.tag_name))
-  });
-
-  response.send(JSON.stringify(data));
-});
-
-exports.scheduled = functions.pubsub
-  .schedule("every 1 minutes")
-  .onRun((context) => {
-    console.log("This will be run every 5 minutes!");
-    functions.logger.info("hello");
-    return null;
+exports.refreshReleasesScheduled = functions.region('europe-west1').pubsub
+  .schedule("every 3 hours")
+  .onRun(async () => {
+    await getNewReleases();
   });
 
 async function asyncForEach(array, callback) {
@@ -44,3 +29,55 @@ async function asyncForEach(array, callback) {
     await callback(array[index], index, array);
   }
 }
+
+const getNewReleases = async () => {
+  const { data, error } = await supabase.from("topic").select("*");
+
+  await asyncForEach(data, async (topic) => {
+    const scmUrlSplit = topic.info.scm.url.split("/");
+    const owner = scmUrlSplit[scmUrlSplit.length - 2];
+    const repo = scmUrlSplit[scmUrlSplit.length - 1];
+
+    const { data } = await octokit.repos.listReleases({
+      owner: owner,
+      repo: repo,
+    });
+
+    const githubReleases = data.map((release) => {
+      const version = release.tag_name.replace("v", "");
+
+      return {
+        info: {
+          name: release.name,
+          publishedAt: release.published_at,
+          version: version,
+          releaseNotesUrl: release.html_url,
+          prerelease: release.prerelease,
+        },
+        topic: topic.id,
+      };
+    });
+
+    const { data: releasesFromSupabase, error } = await supabase
+      .from("release")
+      .select("info->>version")
+      .eq("topic", topic.id)
+      .in(
+        "info->>version",
+        githubReleases.map((it) => it.info.version)
+      );
+
+    const releasesNotInDatabaseYet = githubReleases.filter(
+      (release) =>
+        !releasesFromSupabase.some((it) => it.version === release.info.version)
+    );
+
+    if (releasesNotInDatabaseYet.length) {
+      functions.logger.info("Saving new releases", releasesNotInDatabaseYet);
+    }
+
+    await supabase.from("release").insert(releasesNotInDatabaseYet);
+
+    // release type: stable, beta, milestone, release candidate, eap
+  });
+};
