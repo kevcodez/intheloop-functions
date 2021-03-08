538def4be232fc36b12d4ea9b1ea46896dd23d9b
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2021-03-08 22:07:59 +0100

    Fix language detection by decoding html entities

--- functions/languageDetection.js
+++ functions/languageDetection.js
@@ -1,5 +1,6 @@
 const { Translate } = require("@google-cloud/translate").v2;
 const functions = require("firebase-functions");
+const { decode } = require("html-entities");
 
 const translate = new Translate({
   key: functions.config().googlecloud.api_key,
@@ -7,12 +8,12 @@ const translate = new Translate({
 
 async function detectLanguage(text) {
   const strippedText = text.replace(/(<([^>]+)>)/gi, "");
-  const detections = await translate.detect(strippedText);
+  const htmlEntitiesDecoded = decode(strippedText);
+  const detections = await translate.detect(htmlEntitiesDecoded);
   const detectionsArray = Array.isArray(detections) ? detections : [detections];
   if (detectionsArray.length) {
     return detectionsArray[0].language;
   } else {
-    functions.logger.warn("Could not detect language", { text, detections });
     return null;
   }
 }
